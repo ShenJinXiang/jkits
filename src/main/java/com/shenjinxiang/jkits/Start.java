117735cbf349a54bff8513ddfefb1ad625955956
shenjinxiang <sjx-sword@163.com>
2020-01-16 08:07:43 +0800

    move

--- src/main/java/com/shenjinxiang/jkits/Start.java
+++ src/main/java/com/shenjinxiang/jkits/Start.java
@@ -1,10 +1,6 @@
 package com.shenjinxiang.jkits;
 
-import java.io.File;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.security.CodeSource;
-import java.security.ProtectionDomain;
+import com.shenjinxiang.jkits.kit.StrKit;
 
 public class Start {
 
@@ -18,30 +14,10 @@ public class Start {
 //            }
 //        }, 0, 1000);
         System.out.println(System.getProperty("user.dir"));
-        System.out.println(getCurrentFilePath());
+        System.out.println(StrKit.getCrrentFilePath());
+        System.out.println(StrKit.getCurrentPath());
         System.out.println(Class.class.getResource("/").getPath());
         System.out.println(Start.class.getResource("/").getPath());
     }
 
-    public static String getCurrentFilePath() throws IllegalAccessException {
-        ProtectionDomain protectionDomain = Start.class.getProtectionDomain();
-        CodeSource codeSource = protectionDomain.getCodeSource();
-        URI location = null;
-        try {
-            location = null == codeSource ? null : codeSource.getLocation().toURI();
-        } catch (URISyntaxException e) {
-            e.printStackTrace();
-        }
-        String path = null == location ? null : location.getSchemeSpecificPart();
-        if (null == path) {
-            throw new IllegalAccessException("unable to determine code source archive");
-        }
-        File root = new File(path);
-        if (!root.exists()) {
-            throw new IllegalAccessException("Unable to determine code source archive from " + root);
-        }
-//        return root.getPath();
-//        return root.getAbsolutePath();
-        return root.getParent();
-    }
 }
--- src/main/java/com/shenjinxiang/jkits/kit/StrKit.java
+++ src/main/java/com/shenjinxiang/jkits/kit/StrKit.java
@@ -1,8 +1,45 @@
 package com.shenjinxiang.jkits.kit;
 
+import com.shenjinxiang.jkits.Start;
+
+import java.io.File;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.security.CodeSource;
+import java.security.ProtectionDomain;
+
 public class StrKit {
 
     public static boolean isBlank(String str) {
         return null == str || "".equals(str);
     }
+
+    public static String getCrrentFilePath() throws IllegalAccessException {
+        return getCurrentFile().getAbsolutePath();
+    }
+
+    public static String getCurrentPath() throws IllegalAccessException {
+        return getCurrentFile().getParent();
+    }
+
+    private static File getCurrentFile() throws IllegalAccessException {
+        ProtectionDomain protectionDomain = Start.class.getProtectionDomain();
+        CodeSource codeSource = protectionDomain.getCodeSource();
+        URI location = null;
+        try {
+            location = null == codeSource ? null : codeSource.getLocation().toURI();
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
+        String path = null == location ? null : location.getSchemeSpecificPart();
+        if (null == path) {
+            throw new IllegalAccessException("unable to determine code source archive");
+        }
+        File root = new File(path);
+        if (!root.exists()) {
+            throw new IllegalAccessException("Unable to determine code source archive from " + root);
+        }
+        return root;
+    }
+
 }
